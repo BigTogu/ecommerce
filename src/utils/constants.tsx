feca50335993e5a26ee07b36e205fa1bdcc32bbb
Silvia Kenaan <silvia.kenaan@gmail.com>
2025-05-15 19:37:35 +0200

    refactor: delete dribble

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,7 +1,6 @@
 export const CONTACT_EMAIL = "hola@loucobos.com";
 
 export const SOCIAL_LINKS = [
-  { label: "DRIBBLE", href: "#" },
   { label: "LINKEDIN", href: "https://www.linkedin.com/in/loucobos/" },
   { label: "BEHANCE", href: "https://www.behance.net/loucobos" },
   { label: "INSTAGRAM", href: "https://www.instagram.com/loucobos/" },
